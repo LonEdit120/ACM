04552ee642bb0a4f806d3c4c41f731d86a5e2dd0
skywater860620 <bladeath860620@gmail.com>
2016-03-02 18:43:24 +0800

    3/2 ACM class starting time

---
+++ Hw1_2/UVA/UVA-10026.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct ratio
+{
+	float rate;
+	int locate;
+};
+
+bool cmp(ratio r1, ratio r2)
+{
+	if(r1.rate < r2.rate)
+		return 1;
+	else
+		return 0;
+}
+
+int main()
+{
+	int cases;
+	float days[1001] = {0}, fine[1001] = {0};
+	int count = 0;
+	int lines;
+	int i;
+	ratio r[1001];
+	cin >> cases;
+	{
+		while(count != cases)
+		{
+			count++;
+			cin >> lines;
+			for(i=0; i<lines; i++)
+			{
+				cin >> days[i] >> fine[i];
+				r[i].rate = days[i] / fine[i];
+				r[i].locate = i;
+			}
+			sort(r, r+lines, cmp);
+			for(i=0; i<lines; i++)
+			{
+				if(i != lines-1)	
+					cout << r[i].locate+1 << " ";
+				else
+					cout << r[i].locate+1 << endl;
+			}
+		}
+	}
+}
